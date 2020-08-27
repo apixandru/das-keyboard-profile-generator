f1ea7ce2432ca49257423e6cb70c2301bd2eefc8
apixandru <contact@apixandru.com>
2020-08-27 08:51:19 -0700

    prepare utils

--- src/index.js
+++ src/index.js
@@ -126,7 +126,7 @@ function shoes(value) {
             let allColors = all_colors[keyEnum.key];
             if (allColors) {
                 allColors.color = color;
-                allColors.inverse = invertColor(color);
+                allColors.inverse = invertRgbHex(color);
             }
             const element = elementsByKeys[keyEnum.key];
             setKeyColor(element, color, keyEnum);
@@ -168,31 +168,6 @@ function colors() {
             };
         });
     return elementsByKeys;
-}
-
-function invertColor(hex) {
-    if (hex.indexOf('#') === 0) {
-        hex = hex.slice(1);
-    }
-    // convert 3-digit hex to 6-digits.
-    if (hex.length === 3) {
-        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-    }
-    if (hex.length !== 6) {
-        throw new Error('Invalid HEX color.');
-    }
-    // invert color components
-    var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
-        g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
-        b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
-    // pad each with zeros and return
-    return '#' + padZero(r) + padZero(g) + padZero(b);
-}
-
-function padZero(str, len) {
-    len = len || 2;
-    var zeros = new Array(len).join('0');
-    return (zeros + str).slice(-len);
 }
 
 const defaultBoxShadows = {
---
+++ src/utils.js
@@ -0,0 +1,39 @@
+function base64ToArrayBuffer(base64) {
+    var binary_string = window.atob(base64);
+    var len = binary_string.length;
+    var bytes = new Uint8Array(len);
+    for (var i = 0; i < len; i++) {
+        bytes[i] = binary_string.charCodeAt(i);
+    }
+    return bytes;
+}
+
+function rgbToHex(r, g, b) {
+
+    return '#' + pad(r) + pad(g) + pad(b);
+}
+
+function invertRgbHex(hex) {
+    if (hex.indexOf('#') === 0) {
+        hex = hex.slice(1);
+    }
+    // convert 3-digit hex to 6-digits.
+    if (hex.length === 3) {
+        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    if (hex.length !== 6) {
+        throw new Error('Invalid HEX color.');
+    }
+    // invert color components
+    var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
+        g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
+        b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+    // pad each with zeros and return
+    return '#' + padHex(r) + padHex(g) + padHex(b);
+}
+
+function padHex(str) {
+    return ('0' + str.toString(16))
+        .slice(-2);
+}
+
